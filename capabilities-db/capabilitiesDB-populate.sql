e093b97b16d4d2b8d7782be3cf108f134dbbebf0
Clare Buckley <clarebu@kainos.com>
2019-09-10 11:34:00 +0100

    fix database population script

--- capabilities-db/capabilitiesDB-populate.sql
+++ capabilities-db/capabilitiesDB-populate.sql
@@ -11,19 +11,17 @@ INSERT INTO `Capability`(`capability_name`, `job_family_id`)
     ('Data Engineering Manager',  2);
  
 INSERT INTO `Band`(`band_name`, `band_competency`) 
-    VALUES 
-('Executive', 'Leadership Community');
+    VALUES ('Executive', 'Competency'), ('Leadership Community', 'Competency');
  
 INSERT INTO `Role`(`role_name`, `role_summary`, `role_training`, `role_responsibilities`, `capability_id`, `band_id`)
     VALUES 
     ('Head Executive of Business Unit', 'Summary', 'Training', 'Responsibility', 1, 1),
-    ('Head Leader of Business Unit', 'Summary', 'Training', 'Responsibility', 1, 2), 
-    ('Sales Executive Director', 'Summary', 'Training', 'Responsibility', 2, 1),
-    ('Sales Leadership Director', 'Summary', 'Training', 'Responsibility', 2, 2);
+    ('Head Leader of Business Unit', 'Summary', 'Training', 'Responsibility', 1, 1), 
+    ('Sales Executive Director', 'Summary', 'Training', 'Responsibility', 2, 2),
+    ('Sales Leadership Director', 'Summary', 'Training', 'Responsibility', 2, 1);
  
 INSERT INTO `User`(`user_f_name`, `user_l_name`, `role_id`)
     VALUES 
     ('Jane One', 'Doe', 1),
     ('Jane Two', 'Doe', 2),
-    ('Jane Three', 'Doe', 3),
-    ('Jane Four', 'Doe', 4);
+    ('Jane Three', 'Doe', 3);
