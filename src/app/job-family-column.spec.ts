84f8bbdf4a6d29a5de47a43b826d8af4a5236122
Alex Owen-Meehan <alexo@tribore.kainos.com>
2019-09-17 11:33:43 +0100

    Started writing tests for job family column

--- src/app/job-family-column.spec.ts
+++ src/app/job-family-column.spec.ts
@@ -1,7 +1,22 @@
+import {Band} from './Band';
+import {JobFamily} from './JobFamily';
+import {DataService} from './data.service';
+import {Capability} from './Capability';
+import {Role} from './Role';
 import { JobFamilyColumn } from './job-family-column';
 
 describe('BandColumn', () => {
   it('should create an instance', () => {
     expect(new JobFamilyColumn()).toBeTruthy();
   });
+
+  it('should have the correct capabilities as children', () => {
+
+  	new JobFamily = 
+  	new JobFamilyColumn() = testJobFamilyColumn;
+  	new DataService() = testDataService;
+  	testJobFamilyColumn.populateChildren(testDataService);
+
+  	expect()
+  })
 });
